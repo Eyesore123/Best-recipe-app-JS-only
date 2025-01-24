bf2c9c975c6315930cf1e1bab7738519cf501e18
Eyesore123 <joni.putkinen@protonmail.com>
2025-01-24 11:32:14 +0200

    first commit for a recipe app that helps you create and maintain the reci pes of your favorite foods

--- src/auth.js
+++ src/auth.js
@@ -4,12 +4,7 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebas
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCb7jiH7x7SWhKoGb_S1YRC9tm1bt9jINI",
-  authDomain: "fir-exercise-31abd.firebaseapp.com",
-  projectId: "fir-exercise-31abd",
-  storageBucket: "fir-exercise-31abd.firebasestorage.app",
-  messagingSenderId: "162981277106",
-  appId: "1:162981277106:web:c50158af7056a5899388ab"
+  
 };
 
 // Initialize Firebase
